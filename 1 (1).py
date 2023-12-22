fc542395ce0ac53943833e1a31362b6bfff70495
даня <147221444+Danissemo15@users.noreply.github.com>
2023-12-22 20:13:36 +0700

    Add files via upload

---
+++ 1 (1).py
@@ -0,0 +1,9 @@
+import math
+
+a = float(input('Введите a: '))
+
+z1 = 2*sin**2*(3*math.pi-2*a)*cos**2(5*math.pi+2*a)
+z2 = 1/4 - 1/4*sin(5/2 * math.pi - 8 * a)
+
+print (f'z1 = {z1}')
+print (f'z2 = {z2}')
